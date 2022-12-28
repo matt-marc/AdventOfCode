c837542c51710b16b5e87e007638d91ceab2b6ef
Mathieu Marchildon <mmarc039@uottawa.ca>
2022-12-28 18:31:41 -0500

    some bfs attempts

--- 2022/day15/p2.py
+++ 2022/day15/p2.py
@@ -26,16 +26,16 @@ lines[:] = [line.strip() for line in lines]
 sb_list = []
 beacs = []
 
-x_min = 0
-x_max = 20
-y_min = 0
-y_max = 20
 
-x_min = 3000000
-x_max = 4000000
-y_min = 3000000
-y_max = 4000000
+x_min_range = 0
+x_max_range = 20
+y_min_range = 0
+y_max_range = 20
 
+x_min_range = 3000000
+x_max_range = 4000000
+y_min_range = 3000000
+y_max_range = 4000000
 
 x_f = "Sensor at x={}, y={}: closest beacon is at x={}, y={}"
 for l in lines:
@@ -56,62 +56,116 @@ def is_p_beacon(x_p):
     return False
 
 
-def count_in_row(x_lb, x_ub):
+p_vals = []
+
+
+def add_to_perimiter(x, y):
+    if x <= x_max_range and y <= y_max_range and x >= x_min_range and y >= y_min_range:
+        pass
+        p_vals.append([x, y])
+
+
+def find_perimiter_list():
+    for s in sb_list:
+        x_s = s[0][0]
+        y_s = s[0][1]
+
+        x_max = int(s[2] + x_s)
+        y_max = int(s[2] + y_s)
+        x_min = int(x_s - s[2])
+        y_min = int(y_s - s[2])
+        # start at the top
+        # move cw
+        y = y_max + 1
+        x = x_s
+        while x <= x_max:
+            add_to_perimiter(x, y)
+            y = y - 1
+            x = x + 1
+
+        y = y_s
+        x = x_max + 1
+        while y <= y_min:
+            add_to_perimiter(x, y)
+            y = y - 1
+            x = x - 1
+
+        y = y_min - 1
+        x = x_s
+        while x >= x_min:
+            add_to_perimiter(x, y)
+            y = y + 1
+            x = x - 1
+
+        y = y_s
+        x = x_min - 1
+        while y <= y_max:
+            add_to_perimiter(x, y)
+            y = y + 1
+            x = x + 1
+
+    return p_vals
+
+
+def count_in_row(list_pos):
     in_beacon = False
 
-    for x in range(x_lb, x_ub):
-        for y in range(y_min, y_max):
-            x_p = np.array([x, y])
+    for p in list_pos:
+        x = p[0]
+        y = p[1]
+        x_p = np.array([x, y])
 
-            for s in sb_list:
-                sensor_pos = s[0]
-                l1_sensor = s[2]
+        for s in sb_list:
+            sensor_pos = s[0]
+            l1_sensor = s[2]
 
-                l1_point = np.linalg.norm(x_p - sensor_pos, ord=1)
-                if l1_point <= l1_sensor:
-                    in_beacon = True
-                    break
+            l1_point = np.linalg.norm(x_p - sensor_pos, ord=1)
+            if l1_point <= l1_sensor:
+                in_beacon = True
+                break
 
-            if not in_beacon:
-                print(f"found pos {x} - {y}")
-                val = x*4000000 + y
-                print(f"{val=}")
-                return 1
-            else:
-                in_beacon = False
+        if not in_beacon:
+            print(f"found pos {x} - {y}")
+            val = x * 4000000 + y
+            print(f"{val=}")
+            return 1
+        else:
+            in_beacon = False
 
     return 0
 
 
-x_range = x_max - x_min
-dx = int(x_range / 4)
-
-
-if my_rank == 0:
-    print(f"compute range is ({x_min} - {x_max})")
-
-
-if my_rank == 0:
-    # print(f"running {my_rank=}")
-    x_lb = x_min
-    x_ub = x_min + dx
-    print(f"rank {my_rank} will compute values from {x_lb} to {x_ub}")
-    count_in_row(x_lb, x_ub)
-elif my_rank == 1:
-    # print(f"running {my_rank=}")
-    x_lb = x_min + dx
-    x_ub = x_min + 2 * dx
-    print(f"rank {my_rank} will compute values from {x_lb} to {x_ub}")
-    count_in_row(x_lb, x_ub)
-elif my_rank == 2:
-    # print(f"running {my_rank=}")
-    x_lb = x_min + 2 * dx
-    x_ub = x_min + 3 * dx
-    print(f"rank {my_rank} will compute values from {x_lb} to {x_ub}")
-    count_in_row(x_lb, x_ub)
-elif my_rank == 3:
-    # print(f"running {my_rank=}")
-    x_lb = x_min + 3 * dx
-    x_ub = x_max
-    print(f"rank {my_rank} will compute values from {x_lb} to {x_ub}")
-    count_in_row(x_lb, x_ub)
+p_list = find_perimiter_list()
+count_in_row(p_list)
+# x_range = x_max - x_min
+# dx = int(x_range / 4)
+#
+#
+# if my_rank == 0:
+#    print(f"compute range is ({x_min} - {x_max})")
+#
+#
+# if my_rank == 0:
+#    # print(f"running {my_rank=}")
+#    x_lb = x_min
+#    x_ub = x_min + dx
+#    print(f"rank {my_rank} will compute values from {x_lb} to {x_ub}")
+#    count_in_row(x_lb, x_ub)
+# elif my_rank == 1:
+#    # print(f"running {my_rank=}")
+#    x_lb = x_min + dx
+#    x_ub = x_min + 2 * dx
+#    print(f"rank {my_rank} will compute values from {x_lb} to {x_ub}")
+#    count_in_row(x_lb, x_ub)
+# elif my_rank == 2:
+#    # print(f"running {my_rank=}")
+#    x_lb = x_min + 2 * dx
+#    x_ub = x_min + 3 * dx
+#    print(f"rank {my_rank} will compute values from {x_lb} to {x_ub}")
+#    count_in_row(x_lb, x_ub)
+# elif my_rank == 3:
+#    # print(f"running {my_rank=}")
+#    x_lb = x_min + 3 * dx
+#    x_ub = x_max
+#    print(f"rank {my_rank} will compute values from {x_lb} to {x_ub}")
+#    count_in_row(x_lb, x_ub)
--- 2022/day16/p1.py
+++ 2022/day16/p1.py
@@ -17,7 +17,9 @@ lines[:] = [line.strip() for line in lines]
 
 
 graph = {}
-weights = {}
+flow_rate = {}
+opened = {}
+valve_list = []
 
 
 x_f = "rate={};"
@@ -30,31 +32,111 @@ for l in lines:
     flowrate = ps.parse(x_f, flowrate_s).fixed[0]
     valves = l_list[9:]
 
-    print(f"node {node} has flow rate {flowrate} and is connected to {valves}")
+    # print(f"node {node} has flow rate {flowrate} and is connected to {valves}")
 
+    valve_list.append(node)
     graph[node] = valves
-    weights[node] = flowrate
+    flow_rate[node] = int(flowrate)
+    opened[node] = False
 
 
-def find_p(start_key):
-    mins = 30
-    c_key = start_key
+def find_all_steps(c_key, opened_valves, time):
 
-    while mins > 0:
-        # walk to valve
-        mins = mins - 1
-        c_flow = weights[c_key]
-        c_valves = graph[c_key]
+    p_valves = []
+    for v in valve_list:
+        if not opened_valves[v]:
+            p_valves.append(v)
+
+    # while we have unchecked valves:
+    level = 1
+
+    # starts the valve queue at the given index with parent valve set to self
+    valves_q = [[c_key, c_key, level, 0]]
+    valves_l = valves_q
+
+    print(f"total of {p_valves} need to be visited")
+    print(f"\n\n\n")
+
+    while p_valves:
+        print(f"at level {level} we have to visit {p_valves} valves")
+        print(f"valves to visit at level are {valves_l}")
+
+        next_valves = []
+        for v in valves_l:
+            parent = v[0]
+            children = graph[parent]
+            c_level = v[2] + 1
+            # print(f"iter in current level {v=}")
+            # print(f"iter in current0 level {parent=} {children=}")
+            print(children)
+
+            for c in children:
+                if c in p_valves:
+                    weight = flow_rate[c] * (time - c_level - 1)
+                    valves_q.append([c, parent, c_level, weight])
+                    next_valves.append([c, parent, c_level, weight])
+                    p_valves.remove(c)
+
+        # print(f"{valves_q=}")
+        valves_q.sort(key=lambda v: v[3], reverse=True)
+        return valves_q
 
 
-        max_p = weights[c_valves[0]]
-        #find max pressure path
-        for v in c_valves:
-            max_p = max(max_p,weights[v])
+time = 30
+start_v = "AA"
+total_w = 0
+option_stack = [[start_v, None, opened, time, total_w]]
 
-        print(f"at valve {c_key} possible paths are {c_valves} with max flow at {max_p}")
-        input()
+for bfs_option in option_stack:
+    starting_valve = bfs_option[0]
+    parent_option = bfs_option[1]
+    c_opened = bfs_option[2]
+    c_time = bfs_option[3]
+    c_w = bfs_option[4]
+    print(f"starting at option {starting_valve}")
+    input()
 
+    if c_time > 0:
+        all_options = find_all_steps(starting_valve, c_opened, c_time)
 
+        for op in all_options:
+            w = op[3]
+            if w > 0:
+                v = op[0]
+                time_step = op[2]
 
-find_p("DD")
+                new_opened_valves = c_opened
+                new_opened_valves[v] = True
+                new_time = c_time - time_step
+                new_w = c_w + w
+
+                new_option = [
+                    v,
+                    bfs_option,
+                    new_opened_valves,
+                    new_time,
+                    new_w
+                ]
+                option_stack.append(new_option)
+
+            print(f"current options are")
+            print(option_stack)
+            input()
+
+# while time > 0:
+#    all_options = find_all_steps(start_v, c_opened, time)
+#    # pick best answer
+#    for op in all_options:
+#        w = op[3]
+#        if w > 0:
+#            v = op[0]
+#            time_step = op[2]
+#            option_stack.append()
+#
+#    time = time - time_step
+#    total_w += w_step
+#    c_opened[v] = True
+#    start_v = v
+#
+#    print(f"at time {time} and the best option is {valve_turn_off}")
+#    input()
